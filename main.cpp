e2606d0a6e2b69de20976f8c9098a113fc85e4fe
m_cabello <m_cabello@umail.ucsb.edu>
2021-04-12 15:09:40 -0700

    V1

--- main.cpp
+++ main.cpp
@@ -8,22 +8,22 @@ int main(){
     while(true){
         string question;
         int answer;
-        question = "Welcome to the transaction-chain application.... \n 1) Add a transaction to the chain.\n 2) Find the balance of a person.\n 3) Print the chain.\n 4) Exit. \n Which operation do you want to make? (1,2,3,4):\n" ;
+        question = "Welcome to the transaction-chain application.... \n 1) Add a transaction to the chain.\n 2) Find the balance of a person.\n 3) Print the chain.\n 4) Exit. \n Which operation do you want to make? (1,2,3,4):\n";
         cout << question;
         cin >> answer;
         if (answer == 1){
-            cout << "Integer amount of money:\n";
+            cout << "Integer amount of money:"<< endl;
             int amount;
             cin >> amount;
-            cout << "Sender name\n";
+            cout << "Sender name"<< endl;
             string sender;
             cin >> sender;
-            cout << "Reciever name\n";
+            cout << "Reciever name" << endl;
             string reciever;
             cin >> reciever;
             a.add(amount, sender, reciever);
         }else if (answer == 2){
-            cout << "Person name\n";
+            cout << "Person name" << endl;
             string person;
             cin >> person;
             int balance = a.getBalance(person);
@@ -33,7 +33,7 @@ int main(){
         }else if(answer == 4){
             break;
         }else{
-            cout << "wrong operation!";
+            cout << "wrong operation!"<< endl;
         }
     }
     return 0;
